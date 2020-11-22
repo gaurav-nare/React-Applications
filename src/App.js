a7096c69bb175a916910e7c8a3dbce3574afb4b4
gaurav.nare <gaurav.nare@outlook.com>
2020-11-23 01:20:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,19 +2,23 @@ import React from "react";
 
 import "./app.scss";
 
-import { Container } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import ArticleSection from "./Components/Articles/ArticleSection";
 
 const App = () => {
   return (
     <Container className="mt-4">
-      <h1 className="text-danger">Related Articles</h1>
-      <p className="description">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam quis lacus
-        lacus. Mauris commodo eget leo vitae ullamcorper. Proin sed turpis mi.
-        Nam viverra pharetra nibh ultricies rutrum. Integer vulputate tristique
-        pellentesque. Etiam viverra dolor nec.
-      </p>
+      <Row>
+        <Col sm={12} md={9}>
+          <h1 className="text-danger">Related Articles</h1>
+          <p className="description">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam quis
+            lacus lacus. Mauris commodo eget leo vitae ullamcorper. Proin sed
+            turpis mi. Nam viverra pharetra nibh ultricies rutrum. Integer
+            vulputate tristique pellentesque. Etiam viverra dolor nec.
+          </p>
+        </Col>
+      </Row>
       <ArticleSection />
     </Container>
   );
